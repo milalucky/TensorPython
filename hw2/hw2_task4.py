8de54d8d1619ced972ec69fd18278e7cb7321b4e
Milana Zharkevich <ZharkevichMA@gmailcom>
2023-05-15 22:52:14 +0500

    test

--- hw2/hw2_task4.py
+++ hw2/hw2_task4.py
@@ -5,4 +5,6 @@
 file = "C:\python3\m.ail.txt"
 full_name = file.split('\\')[-1]
 name = full_name[:full_name.rfind('.')]
-print(name)
+disk_name = file.split(':')[0]
+directory = file.split('\\')[1]
+print(f'Имя файла: {name}\nИмя диска: {disk_name}\nКорневая папка: {directory}')
