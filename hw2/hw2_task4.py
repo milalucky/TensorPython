46a53b52ab50f5e9fe3be869adba4709da962838
Milana Zharkevich <ZharkevichMA@gmailcom>
2023-05-15 22:47:30 +0500

    Revert "test"
    This reverts commit 8ac2e1220121a3f8b197dbcc0efc5e4b29d59d2e.

--- hw2/hw2_task4.py
+++ hw2/hw2_task4.py
@@ -5,6 +5,4 @@
 file = "C:\python3\m.ail.txt"
 full_name = file.split('\\')[-1]
 name = full_name[:full_name.rfind('.')]
-disk_name = file.split(':')[0]
-directory = file.split('\\')[1]
-print(f'Имя файла: {name}\nИмя диска: {disk_name}\nКорневая папка: {directory}')
+print(name)
